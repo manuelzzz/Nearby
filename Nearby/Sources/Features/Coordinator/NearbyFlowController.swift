2372125cf849a6c7a8555a996e8f312f542f8fe7
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2024-12-14 10:07:20 -0300

    Flow controller for Nearby navigation

--- Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
+++ Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -9,17 +9,27 @@ import Foundation
 import UIKit
 
 class NearbyFlowController {
-
     private var navigationController: UINavigationController?
 
     init() {}
 
     func start() -> UINavigationController? {
         let contentView = SplashView()
-        let startViewController = SplashViewController(contentView: contentView)
-        self.navigationController = UINavigationController(rootViewController: startViewController)
+        let startViewController = SplashViewController(
+            contentView: contentView,
+            delegate: self
+        )
 
+        self.navigationController = UINavigationController(rootViewController: startViewController)
         return navigationController
     }
+}
 
+extension NearbyFlowController: SplashFlowDelegate {
+    func decideNavigationFlow() {
+        let contentView = WelcomeView()
+        let welcomeViewController = WelcomeViewController(contentView: contentView)
+
+        navigationController?.pushViewController(welcomeViewController, animated: true)
+    }
 }
