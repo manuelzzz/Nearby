be93ff7b461dc5991f9e61b244fee2fce519fc90
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2024-12-14 10:09:11 -0300

    UIViewController extension to setup the Constraints

---
+++ Nearby/Sources/Features/Extensions/UiViewController+ext.swift
@@ -0,0 +1,22 @@
+//
+//  UiViewController+ext.swift
+//  Nearby
+//
+//  Created by Manuel Santos Souza on 13/12/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    public func setupContentViewToViewController(contentView: UIView) {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
