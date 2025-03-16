5a5d1a2f822ba6b6b562108475bf3b82e99e418e
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-03-16 12:32:18 -0300

    feat: flowController sending to SplashViewController

--- Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
+++ Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -9,27 +9,29 @@ import Foundation
 import UIKit
 
 class NearbyFlowController {
-    private var navigationController: UINavigationController?
+	private var navigationController: UINavigationController?
 
-    init() {}
+	func start() -> UINavigationController? {
+		let contentView = SplashView()
+		let startViewController = SplashViewController(contentView: contentView, delegate: self)
+		self.navigationController = UINavigationController(rootViewController: startViewController)
 
-    func start() -> UINavigationController? {
-        let contentView = SplashView()
-        let startViewController = SplashViewController(
-            contentView: contentView,
-            delegate: self
-        )
-
-        self.navigationController = UINavigationController(rootViewController: startViewController)
-        return navigationController
-    }
+		return navigationController
+	}
 }
 
 extension NearbyFlowController: SplashFlowDelegate {
-    func decideNavigationFlow() {
-        let contentView = WelcomeView()
-        let welcomeViewController = WelcomeViewController(contentView: contentView)
+	func decideNavigationFlow() {
+		let contentView = WelcomeView()
+		let welcomeViewController = WelcomeViewController(contentView: contentView)
+		welcomeViewController.flowDelegate = self
+		navigationController?.pushViewController(welcomeViewController, animated: true)
+	}
+}
 
-        navigationController?.pushViewController(welcomeViewController, animated: true)
-    }
+extension NearbyFlowController: WelcomeFlowDelegate {
+	func goToHome() {
+		let homeViewController = HomeViewController()
+		navigationController?.pushViewController(homeViewController, animated: true)
+	}
 }
