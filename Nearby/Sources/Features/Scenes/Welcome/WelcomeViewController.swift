ee8c8155f1a26818f022946bcf3181bf315b824b
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-03-16 12:34:17 -0300

    feat: WelcomeView

--- Nearby/Sources/Features/Scenes/Welcome/WelcomeViewController.swift
+++ Nearby/Sources/Features/Scenes/Welcome/WelcomeViewController.swift
@@ -9,30 +9,33 @@ import Foundation
 import UIKit
 
 class WelcomeViewController: UIViewController {
-    let contentView: WelcomeView
+	let contentView: WelcomeView
+	weak var flowDelegate: WelcomeFlowDelegate?
+	init(contentView: WelcomeView) {
+		self.contentView = contentView
+		super.init(nibName: nil, bundle: nil)
 
-    init(contentView: WelcomeView) {
-        self.contentView = contentView
-        super.init(nibName: nil, bundle: nil)
-    }
+		contentView.didTapButton = { [weak self] in
+			self?.flowDelegate?.goToHome()
+		}
+	}
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setup()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setup()
+	}
 
-    private func setup() {
-        self.view.addSubview(contentView)
+	private func setup() {
+		self.view.addSubview(contentView)
+		view.backgroundColor = Colors.gray100
+		setupConstraints()
+	}
 
-        view.backgroundColor = Colors.gray100
-        setupConstraints()
-    }
-
-    private func setupConstraints() {
-        self.setupContentViewToViewController(contentView: contentView)
-    }
+	private func setupConstraints() {
+		self.setupContentViewToViewController(contentView: contentView)
+	}
 }
